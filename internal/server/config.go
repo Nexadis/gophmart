9d2614f2664b4f47f9723f3b6f225925f646d369
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-08-08 12:51:21 +0300

    Добавил конфиг

--- internal/server/config.go
+++ internal/server/config.go
@@ -1,1 +1,33 @@
 package server
+
+import (
+	"flag"
+
+	"github.com/caarlos0/env/v9"
+)
+
+type Config struct {
+	RunAddress           string `env:"RUN_ADDRESS"`
+	DbURI                string `env:"DATABASE_URI"`
+	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS"`
+}
+
+func NewConfig() *Config {
+	c := &Config{}
+	setFlags(c)
+	return c
+}
+
+func setFlags(c *Config) {
+	flag.StringVar(&c.RunAddress, "a", ":8080", "Run Address for server")
+	flag.StringVar(&c.DbURI, "d", "", "Database Uri")
+	flag.StringVar(&c.AccrualSystemAddress, "r", "", "Accrual System Address")
+}
+
+func (c *Config) Parse() error {
+	flag.Parse()
+	if err := env.Parse(c); err != nil {
+		return err
+	}
+	return nil
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,14 @@
 package server
+
+import "github.com/labstack/echo/v4"
+
+type Server struct {
+	e *echo.Echo
+}
+
+func New(config *Config) Server {
+	e := echo.New()
+	return Server{
+		e: e,
+	}
+}
