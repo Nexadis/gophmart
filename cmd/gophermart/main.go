c2e34b2a18d9871c1cfe019f1fcbea02ca569ac5
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-08-08 12:57:53 +0300

    Написал запуск сервера

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/Nexadis/gophmart/internal/server"
+)
+
+func main() {
+	config := server.NewConfig()
+	config.Parse()
+	s := server.New(config)
+	log.Fatal(s.Run())
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -3,12 +3,22 @@ package server
 import "github.com/labstack/echo/v4"
 
 type Server struct {
-	e *echo.Echo
+	e      *echo.Echo
+	config *Config
 }
 
 func New(config *Config) Server {
 	e := echo.New()
 	return Server{
-		e: e,
+		e:      e,
+		config: config,
 	}
+}
+
+func (s *Server) Run() error {
+	s.MountHandlers()
+	return s.e.Start(s.config.RunAddress)
+}
+
+func (s *Server) MountHandlers() {
 }
