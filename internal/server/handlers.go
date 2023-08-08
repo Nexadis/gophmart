33b0412b1877b1c81d162cbb53398936e233f6ff
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-08-08 13:53:11 +0300

    Заглушки для хендлеров

---
+++ internal/server/handlers.go
@@ -0,0 +1,31 @@
+package server
+
+import "github.com/labstack/echo/v4"
+
+func (s *Server) UserRegister(c echo.Context) error {
+	return nil
+}
+
+func (s *Server) UserLogin(c echo.Context) error {
+	return nil
+}
+
+func (s *Server) UserOrdersSave(c echo.Context) error {
+	return nil
+}
+
+func (s *Server) UserOrdersGet(c echo.Context) error {
+	return nil
+}
+
+func (s *Server) UserBalance(c echo.Context) error {
+	return nil
+}
+
+func (s *Server) UserBalanceWithdraw(c echo.Context) error {
+	return nil
+}
+
+func (s *Server) UserWithdrawals(c echo.Context) error {
+	return nil
+}
