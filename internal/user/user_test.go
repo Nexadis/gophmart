ea96139b940659f2b0469c3935530b9e1dcf9314
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-08-09 15:38:29 +0300

    Тест для пакета user

---
+++ internal/user/user_test.go
@@ -0,0 +1,45 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type want struct {
+	hash string
+	err  error
+}
+
+type testCase struct {
+	name string
+	User User
+	want want
+}
+
+var hashTests = []testCase{
+	{
+		name: "Simple password",
+		User: User{
+			Login:    "admin",
+			Password: "123451245",
+		},
+		want: want{
+			hash: "",
+			err:  nil,
+		},
+	},
+}
+
+func TestIsValidPassword(t *testing.T) {
+	for _, test := range hashTests {
+		t.Run(test.name, func(t *testing.T) {
+			hash, err := test.User.HashPassword()
+			t.Logf("Hash:%s, len:%d", hash, len(hash))
+			if err != nil {
+				assert.Equal(t, test.want.err, err)
+			}
+			assert.Equal(t, test.User.IsValidPassword(hash), true)
+		})
+	}
+}
